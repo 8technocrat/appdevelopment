2f130996cba545f63d9b885da69236d8c1cf4cca
Akanksha <akankshachauhan804@gmail.com>
2025-08-27 16:35:01 +0530

    changes done

---
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.superhero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.superhero.ui.theme.SuperHeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperHeroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Iron Man",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = " $name!",
+            color = Color.Red,
+            fontSize = 48.sp,
+        )
+
+        Text(
+            text = " the super hero",
+            color = Color.Red,
+            fontSize = 48.sp,
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperHeroTheme {
+        Greeting("Iron Man")
+    }
+}
